43ed7bba2a5a8265942a0c95e1a21493168e18ca
muhkemallgp <muhkmuhkemallathifgp@gmail.com>
2024-02-02 09:55:50 +0700

    createProdService

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    public Product create(Product product);
+    public List<Product> findAll();
+}
---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -0,0 +1,31 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public Product create(Product product){
+        productRepository.create(product);
+        return product;
+    }
+
+    @Override
+    public List<Product> findAll(){
+        Iterator<Product> productIterator = productRepository.findAll();
+        List<Product> allProduct =  new ArrayList<>();
+        productIterator.forEachRemaining(allProduct::add);
+        return allProduct;
+    }
+}
