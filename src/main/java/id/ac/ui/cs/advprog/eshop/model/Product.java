b202322d890008cf7963be214359bee978fac914
muhkemallgp <muhkmuhkemallathifgp@gmail.com>
2024-02-04 16:07:17 +0700

    edit product

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -3,9 +3,16 @@ package id.ac.ui.cs.advprog.eshop.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter @Setter
 public class Product {
     private String productId;
     private String productName;
     private int productQuantity;
+
+    public Product() {
+        // Generate random UUID for product ID
+        this.productId = UUID.randomUUID().toString();
+    }
 }
