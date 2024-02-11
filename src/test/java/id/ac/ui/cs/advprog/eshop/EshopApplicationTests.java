c45c3ba3abf97d7ee01b23c3666805a8864deb1d
muhkemallgp <muhkmuhkemallathifgp@gmail.com>
2024-02-11 16:02:09 +0700

    Feat: add unit test for EshopApps and Product Service

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -1,13 +1,25 @@
 package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class EshopApplicationTests {
 
+	@Autowired
+	private EshopApplication eshopApplication;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(eshopApplication);
 	}
 
+	@Test
+	void mainMethodDoesNotThrowException() {
+		assertDoesNotThrow(() -> EshopApplication.main(new String[] {}));
+	}
 }
