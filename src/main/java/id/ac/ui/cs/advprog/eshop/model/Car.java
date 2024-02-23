248ceaa0a1b99c4458698b7b9a2e711269722d53
muhkemallgp <muhkmuhkemallathifgp@gmail.com>
2024-02-24 05:48:21 +0700

    Feat: init model Car.java

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Car {
+    private String carId;
+    private String carName;
+    private String carColor;
+    private int carQuantity;
+}
