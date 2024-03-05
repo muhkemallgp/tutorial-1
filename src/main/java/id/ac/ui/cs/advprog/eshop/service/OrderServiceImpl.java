abfd41cfc31737e51714fbc786baad736af63a7c
muhkemallgp <muhkmuhkemallathifgp@gmail.com>
2024-03-05 13:28:06 +0700

    [GREEN] Implement OrderRepository class (should be OrderService)

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImpl.java
@@ -6,17 +6,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderRepository orderRepository;
     @Override
-    public Order createOrder(Order order) { return null; }
+    public Order createOrder(Order order) {
+        if (orderRepository.findById(order.getId()) == null) {
+            orderRepository.save(order);
+            return order;
+        }
+        return null;
+    }
     @Override
-    public Order updateStatus(String orderId, String status) { return null; }
+    public Order updateStatus(String orderId, String status) {
+        Order order = orderRepository.findById(orderId);
+
+        if (order != null) {
+            Order newOrder = new Order(order.getId(), order.getProducts(), order.getOrderTime(),
+                    order.getAuthor(), status);
+            orderRepository.save(newOrder);
+            return newOrder;
+        } else {
+            throw new NoSuchElementException("Order not found");
+        }
+    }
     @Override
-    public List<Order> findAllByAuthor (String author) { return null; }
+    public List<Order> findAllByAuthor (String author) {
+        return orderRepository.findAllByAuthor(author);
+    }
     @Override
-    public Order findById(String id) { return null; }
+    public Order findById(String orderId) {
+        return orderRepository.findById(orderId);
+    }
 }
