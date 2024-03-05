268baa8deda7527eddd95d64820f507976207d99
muhkemallgp <muhkmuhkemallathifgp@gmail.com>
2024-03-05 11:26:40 +0700

    [GREEN] Implement OrderRepository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
@@ -9,7 +9,35 @@ import java.util.List;
 @Repository
 public class OrderRepository {
     private List<Order> orderData = new ArrayList<>();
-    public Order save (Order order) { return null; }
-    public Order findById (String id) { return null; }
-    public List <Order> findAllByAuthor (String author) { return null; }
+    public Order save (Order order) {
+        int i = 0;
+        for (Order savedOrder : orderData) {
+            if (savedOrder.getId().equals(order.getId())) {
+                orderData.remove(i);
+                orderData.add(i, order);
+                return order;
+            }
+            i++;
+        }
+
+        orderData.add(order);
+        return order;
+    }
+    public Order findById (String id) {
+        for (Order savedOrder : orderData) {
+            if (savedOrder.getId().equals(id)) {
+                return savedOrder;
+            }
+        }
+        return null;
+    }
+    public List <Order> findAllByAuthor (String author) {
+        List <Order> result = new ArrayList<>();
+        for (Order savedOrder : orderData) {
+            if (savedOrder.getAuthor().equals(author)) {
+                result.add(savedOrder);
+            }
+        }
+        return result;
+    }
 }
--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
@@ -67,7 +67,7 @@ class OrderRepositoryTest {
         assertEquals(order.getId(), findResult.getId());
         assertEquals(order.getOrderTime(), findResult.getOrderTime());
         assertEquals(order.getAuthor(), findResult.getAuthor());
-        assertEquals(order.getStatus(), findResult.getStatus());
+        assertEquals(OrderStatus.SUCCESS.getValue(), findResult.getStatus());
     }
 
     @Test
