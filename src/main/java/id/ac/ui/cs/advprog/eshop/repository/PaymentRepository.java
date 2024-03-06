92e1f752dbee1792d78ed6185a4df8a16b1e358a
muhkemallgp <muhkmuhkemallathifgp@gmail.com>
2024-03-06 11:13:52 +0700

    [RED] Add PaymentRepository skeleton

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -1,2 +1,25 @@
-package id.ac.ui.cs.advprog.eshop.repository;public class PaymentRepository {
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+
+@Repository
+public class PaymentRepository {
+    private List<Payment> payments = new ArrayList<>();
+
+    public Payment save(Payment payment) {
+        return payment;
+    }
+
+    public Payment findById(String id) {
+        return null;
+    }
+
+    public List<Payment> getAllPayments() {
+        return payments;
+    }
 }
